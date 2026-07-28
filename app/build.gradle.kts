1b3a3aa549ca54c2179481953f3cd216f1eb32b0
jaredxwos <hatoandaburedo@gmail.com>
2026-07-28 16:11:13 +0800

    bumped up the app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.jaredxwos.coralie"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
